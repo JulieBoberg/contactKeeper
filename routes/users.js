8fab67ecfe93ec5860e8ee45c76564ba14973c7f
JulieBoberg <43385336+JulieBoberg@users.noreply.github.com>
2019-12-05 16:24:49 -0800

    Register users and alerts

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
 
@@ -34,7 +34,7 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (user) {
-        return res.status(400).json({ msg: "User already exists " });
+        return res.status(400).json({ msg: "User already exists" });
       }
       user = new User({
         name,
@@ -43,20 +43,25 @@ router.post(
       });
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
+
       await user.save();
 
-     const payload = {
-         user: {
-             id: user.id
-         }
+      const payload = {
+        user: {
+          id: user.id
         }
-     jwt.sign(payload, config.get('jwtSecret'), {
-         expiresIn: 360000
-     }, (err, token) => {
-         if(err) throw err;
-         res.json({ token });
-     });
-
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
